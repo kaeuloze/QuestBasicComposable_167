4428ed289187821f2bd75024eabe742f136520b9
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-10-17 11:45:33 +0700

    untuk menampilkan gambar dan text

--- app/src/main/java/com/example/myapplication/TataLetak.kt
+++ app/src/main/java/com/example/myapplication/TataLetak.kt
@@ -138,5 +138,17 @@ fun TataLetakBoxColumnRow(modifier: Modifier) {
                 .background(color = Color.Green),
             contentAlignment = Alignment.Center
         )
+        {
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+            Text(text = "Our Garden",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center))
+        }
     }
 }
